656d0bfd0d18013ba5a8274753e501e42a866370
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-10-26 14:30:05 -0400

    debug

--- models/Product.js
+++ models/Product.js
@@ -17,7 +17,7 @@ Product.init(
       autoIncrement: true,
     },
     product_name: {
-      type: String,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     price: {
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 
+const db = require('./config/connection')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database, then turn on the 
+db.sync ({force:false}).then( res=> {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+})
